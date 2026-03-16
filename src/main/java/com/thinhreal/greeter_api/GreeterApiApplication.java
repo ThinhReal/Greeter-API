c69e945c024729c268d679598b3bdf4f964abba6
Thinh Nguyen <thinhlinhtinh2006@gmail.com>
2026-03-16 16:24:19 +0700

    Initial commit: Spring Boot Greeter API

---
+++ src/main/java/com/thinhreal/greeter_api/GreeterApiApplication.java
@@ -0,0 +1,13 @@
+package com.thinhreal.greeter_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreeterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreeterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinhreal/greeter_api/GreeterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinhreal.greeter_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreeterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
